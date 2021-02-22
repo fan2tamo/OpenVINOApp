98d32bcf5f51c1ee7c9fae32a7b1b8ed97a1a4bd
fan2tamo <fan2tamo@gmail.com>
2021-02-22 21:37:22 +0900

    Add files via upload

--- convertModel.bat
+++ convertModel.bat
@@ -4,7 +4,7 @@ set CurrentPath=%buf:~0,-1%
 set OpenVINOPath="C:\Program Files (x86)\Intel\openvino_2021\"
 set EnvName=""
 
-rem Frameworkは onnxかtfを設定すること
+rem Frameworkは onnxかtfかtf2を設定すること
 set Framework=onnx
 set DataType=FP16
 set ModelName=mnist
@@ -26,6 +26,8 @@ if %framework%==onnx (
     call install_prerequisites_onnx.bat
 ) else if %framework%==tf (
     call install_prerequisites_tf.bat
+) else if %framework%==tf2 (
+    call install_prerequisites_tf2.bat
 ) else (
     echo Frameworkを設定すること！
     pause
@@ -38,9 +40,9 @@ rem model optimizerで学習済みモデルをIR形式に変換
 if %framework%==onnx (
     echo python mo.py --framework %framework% --input_model "%CurrentPath%\%ModelName%.onnx" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
     python mo.py --framework %framework% --input_model "%CurrentPath%\%ModelName%.onnx" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
-) else if %framework%==tf (
-    echo python mo.py --framework %framework% --input_model "%CurrentPath%\%ModelName%.pb" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
-    python mo.py --framework %framework% --input_model "%CurrentPath%\%ModelName%.pb" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
+) else (
+    echo python mo.py --framework tf --input_model "%CurrentPath%\%ModelName%.pb" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
+    python mo.py --framework tf --input_model "%CurrentPath%\%ModelName%.pb" --output_dir "%CurrentPath%\OpenVINOApp\OpenVINOAppCpp\Data\Model" --b 1 --data_type %DataType% --scale_values [255,255,255]
 ) 
 
 :end
